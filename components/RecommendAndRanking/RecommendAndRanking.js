60443fdaaeb58db2918fbe069da2c6013337f757
maojiuxu <auroratx66@gmail>
2023-01-31 11:36:20 +0800

    feat: 封装RecommendAndRanking组件并使用

---
+++ components/RecommendAndRanking/RecommendAndRanking.js
@@ -0,0 +1,30 @@
+// components/RecommendAndRanking/RecommendAndRanking.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      default: '大标题' // 也可以是value属性
+    },
+    subTitle: {
+      type: String,
+      default: '小标题' // 也可以是value属性
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
